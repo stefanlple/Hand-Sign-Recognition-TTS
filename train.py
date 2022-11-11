345e78d95fb1fb0df83f63ac167da720e1ea5f4c
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2022-11-11 10:58:33 +0100

    handdetection

--- train.py
+++ train.py
@@ -1,9 +1,14 @@
 import numpy as np
+from cvzone.HandTrackingModule import HandDetector
 import cv2
 
+
 camera = cv2.VideoCapture(0)
+detector= HandDetector(maxHands=1)
 
 while True:
-    cv2.imshow("Hand_Alphabet_Translator",camera)
-    cv2.waitKey(0)
+    success, img= camera.read()
+    hands, img= detector.findHands(img)
+    cv2.imshow("Hand_Alphabet_Translator",img)
+    cv2.waitKey(1)
     cv2.destroyAllWindows()
