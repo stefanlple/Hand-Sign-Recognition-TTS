95d7e74022ad8fa2b1fba4cf3e878e5410708349
VinzentPilz <81297495+VinzentPilz@users.noreply.github.com>
2022-12-05 17:55:04 +0100

    send wav file from python to js via websocket

--- Audio/textToSpeech.js
+++ Audio/textToSpeech.js
@@ -1,21 +1,76 @@
-let context = new AudioContext();
-var sound = new Audio("../speech.mp3");
-let source = context.createMediaElementSource(sound);
-let gain = context.createGain();
-let isPlaying = false;
+/* Websocket */
+const webSocket = new WebSocket("ws://localhost:8765");
 
-source.connect(gain);
-gain.connect(context.destination);
+webSocket.addEventListener("open", () => {
+    console.log("We are connected!");
 
-gain.gain.value = 1;
+    webSocket.send("connected");
+});
+
+webSocket.addEventListener("message", e => {
+    console.log(e);
+    if (typeof(e.data) !== "string") {
+       //console.log(e.data); //blob data
+
+       temp = e.data.text().then((value) => { //wait for the value of the text component from the blob to be read
+        
+        //console.log(value);           //undecoded base64 text
+        let decodedText = atob(value);  //decodes base64
+        //console.log(decodedText);     //decoded bas64 text
+
+        let stringHexArray = (convertToHex(decodedText)).split(" "); //array of wav file with hex values as strings
+
+        for (let i = 1; i < stringHexArray.length; i++) {
+            let hex = parseInt(stringHexArray[i], 16); //converts each element from hex values as strings to ints (16 for hex)
+            audioArray[i - 1] = hex; //ignores first element ("")
+        }
+
+        console.log(audioArray); //fully decoded array of the whole wav file (ready to use in WebAudio)
+       });
+    }
+});
+
+function convertToHex(str) {
+    var hex = '';
+    for(var i=0;i<str.length;i++) {
+        hex += ' '+str.charCodeAt(i).toString(16);
+    }
+    return hex;
+}
+
+/* Web Audio */
+var context = new AudioContext();
+let audioArray = [];
+var buf; // Audio buffer
+
+
+
+function playByteArray(byteArray) {
+
+    var arrayBuffer = new ArrayBuffer(byteArray.length);
+    var bufferView = new Uint8Array(arrayBuffer);
+    for (i = 0; i < byteArray.length; i++) {
+      bufferView[i] = byteArray[i];
+    }
+
+    context.decodeAudioData(arrayBuffer, function(buffer) {
+        buf = buffer;
+        play();
+    });
+}
+
+// Play the loaded file
+function play() {
+    // Create a source node from the buffer
+    var source = context.createBufferSource();
+    source.buffer = buf;
+    // Connect to the final output node (the speakers)
+    source.connect(context.destination);
+    // Play immediately
+    source.start(0);
+}
 
 document.querySelector("#playStopButton").addEventListener("click", function(e) {
-    if (isPlaying) {
-        sound.pause();
-        e.target.innerHTML = "Play";
-    } else {
-        sound.play();
-        e.target.innerHTML = "Stop";
-    }
-    isPlaying = !isPlaying;
+   playByteArray(audioArray);
 });
+
--- tts.py
+++ tts.py
@@ -1,12 +1,39 @@
 import pyttsx3
 #pip install pyttsx3
 #bei Fehler in sapi5.py token hardcoden
+
+import websockets
+import asyncio
+#pip install websockets
+
+import base64
+
+b64file = base64.b64encode(open("speech.wav", "rb").read()) #encode wav file with base64 for sending
+#print(b64file)                      #base64 encoded wav file
+#print(base64.b64decode(b64file))    #base64 decoded wav file ( same as print(open("speech.wav", "rb").read()) )
+
+async def handleMessage(websocket):
+    async for message in websocket:
+        if (message == "connected"):
+            print("New connection")
+            await websocket.send("Hello new client")
+            await websocket.send(b64file)
+
+        print("Client: " + message)
+
+async def main():
+    async with websockets.serve(handleMessage, "localhost", 8765):
+        await asyncio.Future()
+
+asyncio.run(main())
+"""
 engine = pyttsx3.init()
 
 #voices = engine.getProperty('voices')
 #for voice in voices:
     #print(voice.id) #alle verfügbaren Stimmen
 
-engine.say("good morning")
-engine.save_to_file("good morning", 'speech.mp3')
+#engine.say("good morning")
+engine.save_to_file("good morning", 'speech.wav')
 engine.runAndWait()
+"""
