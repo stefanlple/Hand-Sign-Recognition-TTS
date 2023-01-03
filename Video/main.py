05bd936bddb0e489bb50a291617509a1acd3fb35
Stefan Le <81301569+stefanlple@users.noreply.github.com>
2023-01-03 23:01:14 +0100

    added letter on main

--- Video/main.py
+++ Video/main.py
@@ -16,22 +16,24 @@ import string
 
 camera = cv2.VideoCapture(0)
 detector= cvzone.HandTrackingModule.HandDetector(detectionCon=0.8,maxHands=1)
-fpsReader = cvzone.FPS()
 classifier= Classifier("Model/keras_model.h5","AlphabetReferences/classes.txt")
+fpsReader = cvzone.FPS()
 
 offset=20
 imgSize=800
 
 classes= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', "Space"]
 
+message=""
+
 while camera.isOpened():
     #print(randomString(10))
     success, img= camera.read()
+    mainImg=img.copy()
     hands, img= detector.findHands(img)
     fps, img = fpsReader.update(img,pos=(50,80),color=(0,255,0),scale=5,thickness=5)
     whiteImage = np.zeros((imgSize, imgSize, 3), np.uint8)
     whiteImage[:] = (255,255,255)
-
     if hands:
         landmarksPoints= hands[0]
         x,y,width,height= hands[0]["bbox"]
@@ -42,37 +44,45 @@ while camera.isOpened():
         
 
         aspectRatio= height/width
-
-        if aspectRatio>1:
-            k=imgSize/height
-            calculatedWidth=math.ceil(k*width)
-            imgResize= cv2.resize(onlyHand,(calculatedWidth,imgSize))
-            imageResized= imgResize.shape
-            widthGap=math.ceil((imgSize-calculatedWidth)/2)
-            whiteImage[:,widthGap:calculatedWidth+widthGap]=imgResize
-            prediction, index= classifier.getPrediction(whiteImage)
-            print(prediction,index)
-        
-        else:
-            k=imgSize/width
-            calculatedHeight=math.ceil(k*height)
-            imgResize= cv2.resize(onlyHand,(imgSize,calculatedHeight))
-            imageResized= imgResize.shape
-            heightGap=math.ceil((imgSize-calculatedHeight)/2)
-            whiteImage[heightGap:calculatedHeight+heightGap,:]=imgResize
-            prediction, index= classifier.getPrediction(whiteImage)
-            print(prediction,index)
+        try:
+            if aspectRatio>1:
+                k=imgSize/height
+                calculatedWidth=math.ceil(k*width)
+                imgResize= cv2.resize(onlyHand,(calculatedWidth,imgSize))
+                imageResized= imgResize.shape
+                widthGap=math.ceil((imgSize-calculatedWidth)/2)
+                whiteImage[:,widthGap:calculatedWidth+widthGap]=imgResize
+                prediction, index= classifier.getPrediction(whiteImage)
+            
+            else:
+                k=imgSize/width
+                calculatedHeight=math.ceil(k*height)
+                imgResize= cv2.resize(onlyHand,(imgSize,calculatedHeight))
+                imageResized= imgResize.shape
+                heightGap=math.ceil((imgSize-calculatedHeight)/2)
+                whiteImage[heightGap:calculatedHeight+heightGap,:]=imgResize
+                prediction, index= classifier.getPrediction(whiteImage)
+        except:
+            print("resize failed")
+        #print(prediction,index)
         cv2.imshow("White",whiteImage)
         #cv2.imshow("OnlyHand",onlyHand)
+        cv2.putText(mainImg, classes[index],(x+100,y-85), cv2.FONT_ITALIC, 3 , (255,0,0),5)
+        cv2.rectangle(mainImg,(x-offset-50,y-offset-50),(x+offset+width+50,y+offset+height+50),(255,0,0),4)
     #cv2.imshow("White",whiteImage)
-    cv2.imshow("Webcam",img)
+    cv2.imshow("Webcam",mainImg)
 
     
 
     if cv2.waitKey(1) == ord("c"):
         print("close window")
         break
+        
+    if cv2.waitKey(1) == ord(" "):
+        message+=str(classes[index])
     
-
+    if cv2.waitKey(1) == 127: #delete key
+        message[:-1]
+    print(message)
 camera.release()
 cv2.destroyAllWindows()
